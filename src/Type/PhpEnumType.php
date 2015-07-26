81d907d2d2b9fbf065acb5bf017c53557b0797e7
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-07-26 10:36:52 +0200

    Implemented custom type

--- src/Type/PhpEnumType.php
+++ src/Type/PhpEnumType.php
@@ -1,12 +1,35 @@
 <?php
 namespace Acelaya\Doctrine\Type;
 
+use Acelaya\Doctrine\Exception\InvalidArgumentException;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 
-class PhpEnumType extends Type
+/**
+ * Class PhpEnumType
+ * @author Alejandro Celaya Alastrué
+ * @link http://www.alejandrocelaya.com
+ */
+abstract class PhpEnumType extends Type
 {
-    const NAME = 'php_enum';
+    const NAME_PATTERN = 'php_enum_%s';
+
+    protected $enumType = 'MyCLabs\Enum\Enum';
+
+    /**
+     * Gets the name of this type.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return sprintf(self::NAME_PATTERN, $this->getSpecificName());
+    }
+
+    /**
+     * @return string
+     */
+    abstract protected function getSpecificName();
 
     /**
      * Gets the SQL declaration snippet for a field of this type.
@@ -18,26 +41,26 @@ class PhpEnumType extends Type
      */
     public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
     {
-        // TODO: Implement getSQLDeclaration() method.
+        return 'VARCHAR(256) COMMENT "php_enum: values"';
     }
 
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
+        $isValid = call_user_func([$this->enumType, 'isValid'], $value);
+        if (! $isValid) {
+            throw new InvalidArgumentException(sprintf(
+                'The value "%s" is not valid for the enum "%s". Expected one of ["%s"]',
+                $value,
+                $this->enumType,
+                implode('", "', call_user_func([$this->enumType, 'keys']))
+            ));
+        }
 
+        return new $this->enumType($value);
     }
 
     public function convertToDatabaseValue($value, AbstractPlatform $platform)
     {
-
-    }
-
-    /**
-     * Gets the name of this type.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return self::NAME;
+        return (string) $value;
     }
 }
