ed130afb29eed4c6c5e36cd35f2b53dd0da7dabc
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-03-05 10:08:50 +0100

    Created another test enum

---
+++ tests/Enum/Gender.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace Acelaya\Test\Doctrine\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class Gender
+ * @author
+ * @link
+ *
+ * @method static Gender MALE()
+ * @method static Gender FEMALE()
+ */
+class Gender extends Enum
+{
+    const MALE = 'male';
+    const FEMALE = 'female';
+}
