13b8ed3fb63103f527f25861d343f26e1183e790
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-03-05 10:09:16 +0100

    Removed strict types added by mistake

--- src/Type/PhpEnumType.php
+++ src/Type/PhpEnumType.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types = 1);
-
 namespace Acelaya\Doctrine\Type;
 
 use Acelaya\Doctrine\Exception\InvalidArgumentException;
--- tests/Enum/Gender.php
+++ tests/Enum/Gender.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types = 1);
-
 namespace Acelaya\Test\Doctrine\Enum;
 
 use MyCLabs\Enum\Enum;
